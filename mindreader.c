3e5abdf007aa1fa4fc04a6f30d7e8655614d499e
At0micSupra <43355748+At0micSupra@users.noreply.github.com>
2023-11-09 22:24:31 -0700

    added a (slightly) more intelligent mem reader using dynamic memory allocation

--- mindreader.c
+++ mindreader.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 
 /*
@@ -17,5 +18,26 @@ void stupid() {
 
 
 int main() {
-	stupid();
+	//stupid();
+	printf("LOLZ IM READING ALL UR SECRET ARE BELONG TO ME\n\n");
+	// because if we malloc the same size right after freeing, it will give us the same data,
+	// we will try getting more and more data each time. hopefully this will lead to fresh data!
+	int WINDOW_SIZE = 16;
+	while(1) {
+		// allocate a new portion of ram
+		// and find the next 128 chars
+		char* ram = (char*) malloc(WINDOW_SIZE * sizeof(char));
+		if(ram == NULL) {
+			printf("your computer dun fucked up!");
+			return 69;
+		}
+		printf("\n\x1B[31mtesting with %d access window size now...\x1B[0m\n", WINDOW_SIZE);
+		for(int i = 0; i < WINDOW_SIZE; i++) {
+			putchar(ram[i]);
+			usleep(10);
+		}
+		WINDOW_SIZE++;
+		free(ram); // freeing all your rams, go download more!
+		usleep(100);
+	}
 }
